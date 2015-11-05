c6559d6466265541c976937d18b9b33e8b4146f1
Miroslav Jiřík <mjirik@kky.zcu.cz>
2015-11-05 23:02:06 +0100

    rename build dir

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ $PYTHON setup.py install
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
 
-mkdir build
-cd build
+mkdir build_cmake
+cd build_cmake
 CMAKE_GENERATOR="Unix Makefiles"
 # CMAKE_ARCH="-m"$ARCH
 

b5a2b8e1d57cb9890418e6ab5b7f2f1565cbc45c
Miroslav Jiřík <mjirik@kky.zcu.cz>
2015-11-05 22:55:44 +0100

    build with so lib

--- build.sh
+++ build.sh
@@ -7,3 +7,12 @@ $PYTHON setup.py install
 # See
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
+
+mkdir build
+cd build
+CMAKE_GENERATOR="Unix Makefiles"
+# CMAKE_ARCH="-m"$ARCH
+
+cmake .. -G"$CMAKE_GENERATOR" -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake --build .
+cmake --build . --target install

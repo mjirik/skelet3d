cc92f92128a6120e2629ebabb071b71937763cc4
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-05-30 08:27:40 +0200

    Bump version: 1.4.19 → 1.4.20

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if cmake_pkgdir[0] == '$':
 else:
     package_dir={ '': cmake_pkgdir }
 
-__VERSION__='1.4.19'
+__VERSION__='1.4.20'
 
 # src_dir = os.path.abspath(__file__)
 # src_dir, fl = os.path.split(src_dir)

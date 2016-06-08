9f8db755123dc3f38782e4175077828fde1255ef
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-06-08 17:10:57 +0200

    example with skeleton analysis

---
+++ examples/example02.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © %YEAR% %USER% <%MAIL%>
+#
+# Distributed under terms of the %LICENSE% license.
+
+"""
+%HERE%
+"""
+
+
+import skelet3d
+import numpy as np
+# Create donut shape
+volume_data = np.zeros([3, 7, 9], dtype=np.int)
+volume_data [:, :, 1:3] = 1
+volume_data [:, 5, 2:9] = 1
+volume_data [:, 0:7, 5] = 1
+skelet = skelet3d.skelet3d(volume_data)
+
+skan = skelet3d.SkeletonAnalyser(skelet, volume_data=volume_data)
+stats = skan.skeleton_analysis()
+print stats

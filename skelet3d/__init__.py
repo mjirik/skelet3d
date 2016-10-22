61ef87e31266db67e7ead0224dfd0f9d38486ec9
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-10-22 15:58:11 +0200

    tree generator moved into teigen package

--- skelet3d/__init__.py
+++ skelet3d/__init__.py
@@ -3,7 +3,5 @@ __all__ = ['skeletonizer', 'skeleton_analyser', 'tree']
 __version__ = "1.5.13"
 
 import skeleton_analyser
-import tree
 from skeleton_analyser import SkeletonAnalyser
-from tree import TreeBuilder
 from skeletonizer import skelet3d
---
+++ tests/export_test.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+logger = logging.getLogger(__name__)
+# import funkcí z jiného adresáře
+import os
+import os.path
+
+from nose.plugins.attrib import attr
+path_to_script = os.path.dirname(os.path.abspath(__file__))
+import unittest
+import numpy as np
+import sys
+
+import skelet3d
+logger.debug(str(dir(skelet3d)))
+logger.debug(skelet3d.__file__)
+# import skelet3d.
+# from skelet3d import TreeBuilder
+import skelet3d.tree
+from skelet3d.tree import TreeBuilder
+
+#
+
+class ExportTest(unittest.TestCase):
+    def setUp(self):
+        self.interactivetTest = False
+    # interactivetTest = True
+
+
+    def test_test_export_to_esofspy(self):
+        """
+        tests export function
+        """
+
+        import skelet3d.vesseltree_export as vt
+        yaml_input = os.path.join(path_to_script, "vt_biodur.yaml")
+        yaml_output = os.path.join(path_to_script, "delme_esofspy.txt")
+        vt.vt2esofspy(yaml_input, yaml_output)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()

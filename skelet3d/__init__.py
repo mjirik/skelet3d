1b20414c9768f26b37c75a3ea3ec0f341491a92d
Miroslav Jirik <miroslav.jirik@gmail.com>
2017-11-21 10:46:55 +0100

    Bump version: 1.5.22 → 1.5.23

--- skelet3d/__init__.py
+++ skelet3d/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['skeletonizer', 'skeleton_analyser', 'tree']
 
-__version__ = "1.5.22"
+__version__ = "1.5.23"
 
 from . import skeleton_analyser
 from skeleton_analyser import SkeletonAnalyser

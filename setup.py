fa584441cba3bbacad7342f66cdbf181b331d378
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-02-24 17:29:43 +0100

    Bump version: 1.5.32 → 1.5.33

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if cmake_pkgdir[0] == '$':
 else:
     package_dir={ '': cmake_pkgdir }
 
-__VERSION__='1.5.32'
+__VERSION__='1.5.33'
 
 # src_dir = os.path.abspath(__file__)
 # src_dir, fl = os.path.split(src_dir)

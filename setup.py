03ad750ae0a8ffa932a8d0e04b1e16acb2959a37
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-10-14 09:11:25 +0200

    Bump version: 1.6.0 → 1.7.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if cmake_pkgdir[0] == '$':
 else:
     package_dir={ '': cmake_pkgdir }
 
-__VERSION__='1.6.0'
+__VERSION__='1.7.0'
 
 # src_dir = os.path.abspath(__file__)
 # src_dir, fl = os.path.split(src_dir)
--- skelet3d/__init__.py
+++ skelet3d/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['skeletonizer', 'skeleton_analyser', 'tree']
 
-__version__ = "1.6.0"
+__version__ = "1.7.0"
 
 from . import skeleton_analyser
 from .skeleton_analyser import SkeletonAnalyser
